4b586ae460583f3c1326448f9ed6368320c96573
Cem Yildiz <cem.yildiz@tracksfortrucks.com>
2021-01-12 17:04:42 +0100

    renamed project name

--- trstem/asgi.py
+++ trstem/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for TurkishSuffixUI project.
+ASGI config for trstem project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TurkishSuffixUI.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trstem.settings')
 
 application = get_asgi_application()
