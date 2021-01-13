4ef3d63a668a00cf68c1cd3faf871bd2120d0119
Cem Yildiz <cem.yildiz@tracksfortrucks.com>
2021-01-13 16:02:19 +0100

    favicon

--- trstem/urls.py
+++ trstem/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from ui import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ui.urls'))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
