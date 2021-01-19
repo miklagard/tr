f7f038d42077c819908570a1e41530c2c751cbb0
Cem Yildiz <cem.yildiz@tracksfortrucks.com>
2021-01-19 15:24:21 +0100

    search engine

--- ui/urls.py
+++ ui/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('nouns/', ui.nouns, name='nouns'),
     path('example/verbs/', ui.example_verbs, name='example_verbs'),
     path('verb/conjunct/', ui.conjunct_verb, name='conjunct_verb'),
-    path('verb/conjunct/<slug:verb>/', ui.conjunct_verb_slug, name='conjunct_verb')
+    path('verb/conjunct/<slug:verb>/', ui.conjunct_verb_slug, name='conjunct_verb'),
+    path('robots.txt', ui.robots, name='robots'),
+    path('sitemap.xml', ui.sitemap, name='sitemap'),
 ]
