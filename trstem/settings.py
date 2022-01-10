cc90cad1f3374649d7c62450d44a32e14d3fada2
Cem Yildiz <cem.yildiz@tracksfortrucks.com>
2022-01-10 13:27:59 +0100

    library upgrade

--- trstem/settings.py
+++ trstem/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'se9*r69=$i-wj0yvrct0w_-2t(mmv^vyt!gnma)_ny+r@a@5y^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('PROD') == 'prod'
+DEBUG = os.environ.get('PROD') != 'prod'
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'www.trstem.com', 'trstem.com']
 
